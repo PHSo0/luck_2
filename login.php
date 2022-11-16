61ccc90ce800f1c0628f9095c70802f794e99762
hyojin0326 <gywlsl0826@naver.com>
2022-11-16 18:45:43 +0900

    로그인 화면

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <header>
+        <span class = "box1">오늘의 운세</span>
+        <span class = "box2">로그인 </span>
+        <span class = "box3">회원가입</span>
+        <span class = "box4">Q&A</span>
+    </header>
+    <p class = "login">LOGIN</p>
+    <input class = "id" type = "text" placeholder="ID">
+    
+</body>
+</html>
