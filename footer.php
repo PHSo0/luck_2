b2a58921d80279a7d467a6d22211a5c8d263a68f
hyojin0326 <gywlsl0826@naver.com>
2022-11-18 02:48:37 +0900

    김효진 - footer-ver1

---
+++ footer.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<style>
+*{
+    padding : 0px;
+    margin : 0px;
+    border : none;
+}
+body{
+    background-color: #fffdf5;
+}
+#wrapper{
+    height : auto;
+    min-height : 100%;
+    padding-bottom: 70px;
+}
+footer{
+    background-color: #442c2c;
+    width :100%;
+    height : 70px;
+    position : absolute;
+    bottom : 0;
+}
+.first{
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 15px;
+    line-height: 20px;
+    letter-spacing: 0.05em;
+    color: #fafafa;
+}
+.second{
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 14px;
+    letter-spacing: 0.05em;
+    color: #fafafa;
+}
+</style>
+</head>
+<body>
+    <div id = "wrapper">
+
+    </div>
+    <footer>
+        <div class = "first">
+        <p>주소 :  08221 서울시 구로구 경인로 445 ([구]고척동 62-160) 동양미래대학교
+        </p>
+        </div>
+        <div>
+        <p class = "second">출처 : https://kor.pngtree.com/freepng/zodiac-cartoon-hand-will-twelve-zodiac-mouse-cattle_3784078.html, https://kor.pngtree.com/freepng/12-constellations-cute-elements-set_4061735.html,https://kor.pngtree.com/freepng/mbe-style-cartoon-weather-icon-elements-collection_3699707.html,https://kor.pngtree.com/freepng/cartoon-mbe-education-icon-, https://kor.pngtree.com/freepng/fortune-cookies-cookie-chinese-traditional_8726946.htmlhttps://kor.pngtree.com/freepng/fortune-cookies-cookie-chinese-traditional_8726946.htmlcollection_5464415.html</p>
+        </div>
+    </footer>
+</body>
+</html>
