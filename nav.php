34a029d5c5d2f0a5e642c4fc70cba53eb17f0335
seominji03 <setg711@gmail.com>
2022-11-20 01:07:33 +0900

    메인 네비바 입니다.

---
+++ nav.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<style>
+body{
+    position : fixed;
+    background-color: #FFFDF5;
+    width:100%;
+    height:70px;
+}
+
+.a{
+    font-family: 'Inter';
+    font-style: normal;
+    font-size: 28px;
+    line-height: 34px;
+    text-align: center;
+    color: #2E1E1E;
+    position: absolute;
+    top:95%;
+    left:1.8%;
+}
+
+.b{
+    font-family: 'Inter';
+    font-style: normal;
+    font-size: 28px;
+    line-height: 34px;
+    text-align: center;
+    color: #2E1E1E;
+    position: absolute;
+    top:95%;
+    left: 20.8%;
+}
+
+.c{
+    font-family: 'Inter';
+    font-style: normal;
+    font-size: 28px;
+    line-height: 34px;
+    text-align: center;
+    color: #2E1E1E;
+    position: absolute;
+    top:95%;
+    left:37%;
+}
+.d{
+    font-family: 'Inter';
+    font-style: normal;
+    font-size: 28px;
+    line-height: 34px;
+    text-align: center;
+    color: #2E1E1E;
+    position: absolute;
+    top:95%;
+    left:55.4%;
+}
+
+.e{
+    font-family: 'Inter';
+    font-style: normal;
+    font-size: 28px;
+    line-height: 34px;
+    text-align: center;
+    color: #2E1E1E;
+    position: absolute;
+    top:95%;
+    left:74%;
+}
+
+.f{
+    font-family: 'Inter';
+    font-style: normal;
+    font-size: 28px;
+    line-height: 34px;
+    text-align: center;
+    color: #2E1E1E;
+    position: absolute;
+    top:95%;
+    left:87.8%;
+}
+</style>
+
+<body>
+    <?php
+        include("no_login_header.php");
+    ?>
+        <span class = "a">오늘의 운세</span>
+        <span class = "b">띠운세</span>
+        <span class = "c">별자리운세</span>
+        <span class = "d">듣고 싶은 말</span>
+        <span class = "e">포춘쿠키</span>
+        <span class = "f">행운의 아이템</span>
+</body>
+</html>
