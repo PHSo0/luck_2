6a1ff0990a2eb946f66f44ad3f3dac5e01aa041a
hyojin0326 <gywlsl0826@naver.com>
2022-11-25 15:22:06 +0900

    듣고 싶은 말 구현

--- main/main_talk.php
+++ main/main_talk.php
@@ -7,13 +7,49 @@
     <title>Document</title>
 </head>
 <style>
-        .content{
+        .content_h{
             background-color: #fffdf5;
             width :100%;
             height: 793px;
             position : absolute;
             top : 123px;
+        }
+        .img_s{
+            margin-top:20px;
+        }
+        img { display:block; margin:auto;}
 
+        .talk_1{
+            width:331px;
+            height:445px;
+            margin-left:157px;
+            margin-top:20px;
+            background:green; 
+            opacity: 0.5;
+        }
+        .talk_2{
+            width:331px;
+            height:445px;
+            margin-left:513px;
+            margin-top:20px;
+            background:yellow;
+            opacity: 0.5;
+        }
+        .talk_3{
+            width:331px;
+            height:445px;
+            margin-left:865px;
+            margin-top:20px;
+            background:red;
+            opacity: 0.5;
+        }
+        .talk_4{
+            width:331px;
+            height:445px;
+            margin-left:1218px;
+            margin-top:20px;
+            background:blue;
+            opacity: 0.5;
         }
     </style>
 <body>
@@ -21,14 +57,30 @@
     session_start();
     if(! isset($_SESSION['id'])){
         include("../header/no_login_header.php");
-        include("../main_nav/talk.php");
-        include("../no_login/no_login_talk.php");
     } else{
         include("../header/yes_login_header.php");
-        include("../main_nav/talk.php");
-        include("main_content/main_talk_content.php");
+    }
+    include("../main_nav/talk.php");
+    if(! isset($_SESSION['id'])){
+        include("../no_login/no_login_ddi.php");
+    } else{
+       ?>
+    <div class="content_h">
+    
+    <img class="img_s" src="../icon/talk/찐완성.png" width="1440px" height="800px">
+    <div class="talk_1"></div>
+    
+    <!-- <div class="talk_1"></div> -->
+    <div class="talk_2"></div>
+    <div class="talk_3"></div>
+    <div class="talk_4"></div>
+    </div>
+    <?php
     }
     ?>
     
+    
+
+    
 </body>
-</html>
+</html> 
