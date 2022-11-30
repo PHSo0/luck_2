9a07d2588cdd8f0eb6e21a3dc5a8fc78a0505188
hyojin0326 <gywlsl0826@naver.com>
2022-12-01 00:05:08 +0900

    게시판 구성

--- Q&A/board.php
+++ Q&A/board.php
@@ -1,65 +1,4 @@
 <!-- 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        #QA_content{
-            height: 863px;
-            width: 77.5%;
-            background-color: #FFFDF5;
-            position: absolute;
-            top: 53px;
-            left: 294px;
-        }
-        .board_1{
-            margin : 160px 320px 0px 320px;
-            width : 50%;
-            height : 84px;  
-            text-align: left;
-            border-top : 1px solid black;
-            border-bottom : 1px solid black;
-            position: relative;
-            top : 100px;
-            display :inline-block;
-        }
-        .board_2{
-            margin : 0px 320px 0px 320px;
-            width : 50%;
-            height : 84px;  
-            text-align: left;
-            border-bottom : 1px solid black;
-            position: relative;
-            top : 100px;
-            display :inline-block;
-        }
-        .post{
-            margin :30px;
-        }
-
-        .sub_button{
-            font-family: 'Inter';
-            font-style: normal;
-            font-weight: 700;
-            font-size: 20px;
-            line-height: 24px;
-            color: #000000;
-            background-color: #FFFDF5;
-            margin : 0px 320px 0px 320px;
-            height: 84px;  
-            position: relative;
-            top : 100px;
-            display : block;
-            margin-left: auto;
-        }
-        
-        
-    </style>
-</head>
-<body>
     session_start();
     if(! isset($_SESSION['id'])){
         include("../header/no_login_header.php");
@@ -121,6 +60,45 @@
             top: 53px;
             left: 294px;
         }
+        .board_1{
+            margin : 160px 320px 0px 320px;
+            width : 100%;
+            height : 84px;  
+            text-align: left;
+            border-top : 1px solid black;
+            border-bottom : 1px solid black;
+            position: relative;
+            top : 100px;
+            display :inline-block;
+        }
+        .board_2{
+            margin : 0px 320px 0px 320px;
+            width : 100%;
+            height : 84px;  
+            text-align: left;
+            border-bottom : 1px solid black;
+            position: relative;
+            top : 100px;
+            display :inline-block;
+        }
+        .post{
+            margin:30px;
+        }
+        .sub_button{
+            font-family: 'Inter';
+            font-style: normal;
+            font-weight: 700;
+            font-size: 20px;
+            line-height: 24px;
+            color: #000000;
+            background-color: #FFFDF5;
+            margin : 0px 320px 0px 320px;
+            height: 84px;  
+            position: relative;
+            top : 100px;
+            display : block;
+            margin-left: auto;
+        }
     </style>
 </head>
 <body>
@@ -135,26 +113,44 @@
     ?>
     <div id = "QA_content">
 
-    <table class="list-table">
+    <table>
     <?php
         require_once('../DB/DB.php');
         $result = $db -> query("SELECT * FROM board ORDER BY no desc");
         $result -> execute();
-        // board테이블에서 idx를 기준으로 내림차순해서 4개까지 표시
         $result = $result->fetch();
-        $total = mysqli_num_rows($result);
-        while($rows = mysqli_fetch_assoc($result)){
+        $count=1;
+        if($result['no']==false){
+
+        }
+        else {
+            for($i=1; $i<=$result['no']; $i++){
         ?>
         <tbody>
-        <tr>
-          <td class = "board_1"><a href="view.php?no=<?php echo $rows['no']?>"><?php echo $rows['Qtitle']?></td>
-        </tr>
         <?php
-                $total--;
-                }
+        $a = $db -> query("SELECT * FROM board where no=$count");
+        $a -> execute();
+        $a = $a->fetch();
+        if($count %4 ==1){
+        ?>
+        <tr>
+          <td class = "board_1" onclick.href="view.php?no=<?php echo $a['no']?>"><p class="post"><?php echo $a['Qtitle']?></p></td>
+        </tr>
+        <?php } 
+        else {
+        ?>
+            <tr>
+            <td class = "board_2" onclick.href="view.php?no=<?php echo $a['no']?>"><p class="post"><?php echo $a['Qtitle']?></p></td>
+          </tr>  
+        <?php
+        }
+        $count++;
+    }
+}
         ?>
     </tbody> 
     </table> 
+    <input type = submit value="글쓰기" class = "sub_button" onclick = "location.href = 'board_submit.php'">
 
 
 
